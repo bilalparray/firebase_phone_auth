0f0a8ac02d2b9d48bd3bd27429ddfa2976b1576d
Bilal Ahmad <dev.renosoftwares@gmail.com>
2025-05-30 17:30:02 +0530

    50mins watched

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
 import 'package:firebase_auth/screens/welcome_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
+///50 mins vidwo watched
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
